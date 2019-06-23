745bbb02c845ceb2063e2ce887bd60be3f9f1d09
wangandshen <2258995676@qq.com>
2019-06-23 20:08:25 +0800

    拓展版

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnClick()
+    {
+        SceneManager.LoadScene("CloseGame");
+    }
+
+}
